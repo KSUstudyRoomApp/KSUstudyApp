34a654b7d94e5afc52ec594a639ccbe30aa08c2b
Gregory White <>
2018-03-13 19:05:51 -0400

    awsconfiguration.json updates, app.gradle updates, and minor changes to the LoginActivity class.

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/LogInActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/LogInActivity.java
@@ -1,5 +1,6 @@
 package com.example.slmns.ksustudyroom;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.models.nosql.UserAccountsDO;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobile.auth.ui.SignInUI;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 
 
@@ -18,6 +25,11 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
  */
 public class LogInActivity extends AppCompatActivity {
 
+    DynamoDBMapper dynamoDBMapper;
+    String userName = "";
+    String Password = "";
+    UserAccountsDO userItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +37,22 @@ public class LogInActivity extends AppCompatActivity {
 
         AWSMobileClient.getInstance().initialize(this).execute();
 
-
         final EditText loginUsername = (EditText) findViewById(R.id.loginUsernameText);
         final EditText loginPassword = (EditText) findViewById(R.id.loginPasswordText);
         final Button loginButton = (Button) findViewById(R.id.loginButton);
         final TextView registerLink = (TextView) findViewById(R.id.loginRegisterTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LogIn(String.valueOf(loginUsername.getText()), String.valueOf(loginPassword.getText()));
+                if (userName.equals(loginUsername) && Password.equals(loginPassword)) {
+                    Intent loginItent = new Intent(LogInActivity.this, RegisterActivity.class);
+                    LogInActivity.this.startActivity(loginItent);
+                }
+            }
+        });
 
         /**
          * Switches to the register page on click.
@@ -41,5 +64,16 @@ public class LogInActivity extends AppCompatActivity {
                 LogInActivity.this.startActivity(regiserIntent);
             }
         });
+
+    }
+
+    public void LogInQuery() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+              
+            }
+        }).start();
     }
 }
