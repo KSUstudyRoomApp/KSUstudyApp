a199d6a88ba0527915925217a214a55448f6fb36
Y-PC\slmns <slmnsmith635@outlook.com>
2018-03-13 16:22:36 -0400

    Added R.A's and Franks screens.

---
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/MariettaCampus.java
@@ -0,0 +1,52 @@
+package com.example.slmns.ksustudyroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MariettaCampus extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marietta_campus);
+
+        //add marietta webview
+        webView = (WebView) findViewById(R.id.mariettaWebView);
+        webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("http://library.kennesaw.edu/services/studyrooms.php");
+        webView.loadUrl("http://kennesaw.libcal.com/booking/studyroomsM");
+
+        //add Websettings like js
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_marietta_campus, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
