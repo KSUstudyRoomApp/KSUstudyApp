6e2b5d13f8ec025ae07ce60323f38d2081fc09de
Y-PC\slmns <slmnsmith635@outlook.com>
2018-04-03 20:30:08 -0400

    Created a new activity called "StartStudyGroupActivity".

---
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/StartStudyGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.slmns.ksustudyroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Handles the page for creating study groups.
+ */
+public class StartStudyGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_study_group);
+    }
+}
