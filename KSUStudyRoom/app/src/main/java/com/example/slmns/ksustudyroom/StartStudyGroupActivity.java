6d61b31ef31cfa84431127aeb8a9a5235cd2ee09
Y-PC\slmns <slmnsmith635@outlook.com>
2018-04-09 19:41:14 -0400

    The added member's username is cleared from the text field when the add member button is pressed.

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/StartStudyGroupActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/StartStudyGroupActivity.java
@@ -2,6 +2,9 @@ package com.example.slmns.ksustudyroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Handles the page for creating study groups.
@@ -10,7 +13,23 @@ public class StartStudyGroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_study_group);
+
+        final Button addMemberButton  = findViewById(R.id.addMemberButton);
+        final TextView addMemberTextBox = findViewById(R.id.addMemberTextBox);
+
+        /**
+         * Add the member and clear the user's name when clicking the add memeber button.
+         */
+        addMemberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMemberTextBox.setText("");
+            }
+        });
+
+
     }
 }
