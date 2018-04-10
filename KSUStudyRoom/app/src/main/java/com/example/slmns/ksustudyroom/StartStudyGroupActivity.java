376d5d328c518afd16d6fbbfedbff185c696e6c5
Y-PC\slmns <slmnsmith635@outlook.com>
2018-04-09 20:25:37 -0400

    Created an ArrayList for holding a list of usernames of the requested users to add to the study group.
    Add on click listener method stubs for the submit and cancel button as placeholders until it is more clear on how to implement the events and still need the pages for the buttons to navigate to.

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/StartStudyGroupActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/StartStudyGroupActivity.java
@@ -6,19 +6,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Handles the page for creating study groups.
  */
 public class StartStudyGroupActivity extends AppCompatActivity {
 
+    private ArrayList groupMembers;
+    public ArrayList getgroupMembers() { return this.groupMembers; }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        this.groupMembers = new ArrayList();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_study_group);
 
         final Button addMemberButton  = findViewById(R.id.addMemberButton);
+        final Button submitStudyGroupButton = findViewById(R.id.submitStudyGroupButton);
+        final Button cancelButton = findViewById(R.id.cancelStudyGroupButton);
         final TextView addMemberTextBox = findViewById(R.id.addMemberTextBox);
+        final TextView confirmAddedUserText = findViewById(R.id.confirmAddedUserText);
 
         /**
          * Add the member and clear the user's name when clicking the add memeber button.
@@ -26,9 +36,27 @@ public class StartStudyGroupActivity extends AppCompatActivity {
         addMemberButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                groupMembers.add(addMemberTextBox.getText());
+                confirmAddedUserText.setText(addMemberTextBox.getText() + " will be added to the group");
                 addMemberTextBox.setText("");
             }
         });
+
+        submitStudyGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
 
     }
