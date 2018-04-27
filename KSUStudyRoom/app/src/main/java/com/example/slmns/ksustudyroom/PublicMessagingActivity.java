8f02c37c65fa61882e768b654e4335713f3c8c0c
Y-PC\slmns <slmnsmith635@outlook.com>
2018-04-27 10:07:18 -0400

    Resized button, and other components in the chat room.

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/PublicMessagingActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/PublicMessagingActivity.java
@@ -12,6 +12,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
 
 import java.util.Date;
 import java.util.HashMap;
