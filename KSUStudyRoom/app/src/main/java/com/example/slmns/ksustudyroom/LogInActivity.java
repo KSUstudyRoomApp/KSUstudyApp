63ca4361be0554e738c36d289a2af63d70e81fd5
Gregory White <gwhite6194@gmail.com>
2018-04-28 17:56:06 -0400

    Got AsyncTask to work
    Now trying to use the results from the AsyncTask to allow or deny access to the application

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/LogInActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/LogInActivity.java
@@ -8,20 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.amazonaws.mobile.client.AWSMobileClient;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -38,50 +36,6 @@ public class LogInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
 
 
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                //get all users api call
-                try {
-                    URL url = new URL("http://ksustudyroom.azurewebsites.net/api/users/getall");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
-                    conn.setRequestMethod("GET");
-                    int responseCode = conn.getResponseCode();
-                    System.out.println("\nSending 'GET' request to URL : " + url);
-                    System.out.println("Response Code : " + responseCode);
-
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(conn.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-                    //print in String
-                    System.out.println(response.toString());
-
-                   JSONArray jsonArray = new JSONArray(response);
-                   //jsonArray.get(1).toString();
-
-
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-            }
-
-        });
-
-
-
 
         final EditText loginUsername = (EditText) findViewById(R.id.loginUsernameText);
         final EditText loginPassword = (EditText) findViewById(R.id.loginPasswordText);
@@ -91,41 +45,99 @@ public class LogInActivity extends AppCompatActivity {
         final TextView invalidLoginUsernNameLabel = (TextView) findViewById(R.id.invalidLoginUserNameLabel);
         final TextView invalidLoginPasswordLabel = (TextView) findViewById(R.id.invalidLoginPasswordLabel);
         final TextView toStudyGroupLink = (TextView) findViewById(R.id.toStudyGroupLink);
+        final String url = "http://ksustudyroom.azurewebsites.net/api/users/getall";
+        final String body = "login?Username="+loginUsername.getText().toString()+"&Password="+loginPassword.getText().toString();
+        final LoginTask loginTask = new LoginTask();
+        JSONArray actualResults = new JSONArray();
 
+        final String jsonString;
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean missingField = false;
-              try
-              {
-                  if(loginUsername.getText().toString().isEmpty()){
-                      invalidLoginUsernNameLabel.setVisibility(View.VISIBLE);
-                      missingField = true;
-                  }
-                  else{
-                      invalidLoginUsernNameLabel.setVisibility(View.INVISIBLE);
-                  }
+                JSONArray loginInfo = null;
 
-                  if(loginPassword.getText().toString().isEmpty()){
-                      invalidLoginPasswordLabel.setVisibility(View.VISIBLE);
-                      missingField = true;
-                  }
-                  else{
-                      invalidLoginPasswordLabel.setVisibility(View.INVISIBLE);
-                  }
 
-                  if(missingField){
-                      throw new Exception();
-                  }
-                  else{
-                      Intent homeIntent = new Intent(LogInActivity.this, HomeV2Activity.class);
-                      LogInActivity.this.startActivity(homeIntent);
-                  }
-              }
-              catch (Exception e){
-                  e.printStackTrace();
-              }
+
+                /*AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        String userName= loginUsername.getText().toString();
+                        String password= loginPassword.getText().toString();
+                        //get all users api call
+                        try {
+                            URL url = new URL("http://ksustudyroom.azurewebsites.net/api/users/login?Username="+userName+"&Password="+password);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+                            conn.setRequestMethod("GET");
+                            int responseCode = conn.getResponseCode();
+                            System.out.println("\nSending 'POST' request to URL : " + url);
+                            System.out.println("Response Code : " + responseCode);
+
+                            //conn.setDoOutput(true);
+                            //OutputStream os = conn.getOutputStream();
+                            //os.write("username=vdoe200".getBytes());
+                            //os.write("password=Test-ksuApp".getBytes());
+                            //os.flush();
+                            //os.close();
+
+                            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+                            String inputLine;
+                            StringBuffer response = new StringBuffer();
+                            while ((inputLine = in.readLine()) != null) {
+                                response.append(inputLine);
+                            }
+                            in.close();
+                            //print in String
+                            System.out.println(response.toString());
+
+                             JSONArray jsonArray = new JSONArray(response);
+                             loginInfo= jsonArray;
+                            //jsonArray.get(1).toString();
+
+
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                });*/
+                while (loginInfo == null) {
+                    try {
+                        loginInfo = loginTask.execute(loginUsername.getText().toString(), loginPassword.getText().toString()).get();
+
+                        if (loginUsername.getText().toString().isEmpty()) {
+                            invalidLoginUsernNameLabel.setVisibility(View.VISIBLE);
+                            missingField = true;
+                        }
+                        else {
+                            invalidLoginUsernNameLabel.setVisibility(View.INVISIBLE);
+                        }
+
+                        if (loginPassword.getText().toString().isEmpty()) {
+                            invalidLoginPasswordLabel.setVisibility(View.VISIBLE);
+                            missingField = true;
+                        }
+                        else {
+                            invalidLoginPasswordLabel.setVisibility(View.INVISIBLE);
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent homeIntent = new Intent(LogInActivity.this, HomeV2Activity.class);
+                LogInActivity.this.startActivity(homeIntent);
 
             }
         });
@@ -141,12 +153,79 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
 
-        toStudyGroupLink.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Intent studyGroupIntent = new Intent(LogInActivity.this,StartStudyGroupActivity.class );
+        toStudyGroupLink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent studyGroupIntent = new Intent(LogInActivity.this, StartStudyGroupActivity.class);
                 LogInActivity.this.startActivity(studyGroupIntent);
             }
         });
 
+
     }
-}
+
+    public class LoginTask extends AsyncTask<String, String, JSONArray> {
+
+
+
+        public static final String REQUEST_METHOD = "GET";
+        public static final int READ_TIMEOUT = 15000;
+        public static final int CONNECTION_TIMEOUT = 15000;
+
+        @Override
+        protected JSONArray doInBackground(String... params){
+            String stringUrl = params[0];
+            JSONArray result = new JSONArray();
+            String inputLine;
+            String userName=params[0];
+            String password=params[1];
+                //get all users api call
+                try {
+                    URL url = new URL("http://ksustudyroom.azurewebsites.net/api/users/login?Username="+userName+"&Password="+password);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    conn.setRequestMethod("GET");
+                    int responseCode = conn.getResponseCode();
+                    System.out.println("\nSending 'POST' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    //conn.setDoOutput(true);
+                    //OutputStream os = conn.getOutputStream();
+                    //os.write("username=vdoe200".getBytes());
+                    //os.write("password=Test-ksuApp".getBytes());
+                    //os.flush();
+                    //os.close();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    //String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    //print in String
+                    System.out.println(response.toString());
+
+                    result = new JSONArray(response);
+                    //loginInfo= jsonArray;
+                    //jsonArray.get(1).toString();
+
+                }
+                catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+        protected void onPostExecute(JSONArray result){
+            super.onPostExecute(result);
+        }
+        }
+
+    }
+
