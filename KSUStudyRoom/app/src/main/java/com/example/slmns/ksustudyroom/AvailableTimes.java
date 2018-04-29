2af109c8ea1100547c4de813e075c8ce9a3ace90
RAKeeling <36350975+RAKeeling@users.noreply.github.com>
2018-04-29 16:54:17 -0400

    Add files via upload

---
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/AvailableTimes.java
@@ -0,0 +1,13 @@
+package com.example.slmns.ksustudyroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvailableTimes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_times);
+    }
+}
