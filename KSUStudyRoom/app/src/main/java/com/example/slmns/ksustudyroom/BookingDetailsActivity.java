0dd440d25689853b4277938d5b50e98ff3169fbe
Gregory White <gwhite6194@gmail.com>
2018-04-30 19:40:34 -0400

    Look at the BookingDetailsActivity page clicklistener

--- KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/BookingDetailsActivity.java
+++ KSUStudyRoom/app/src/main/java/com/example/slmns/ksustudyroom/BookingDetailsActivity.java
@@ -9,7 +9,17 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BookingDetailsActivity extends AppCompatActivity {
     TextView bookingDataTextString;
@@ -39,9 +49,62 @@ public class BookingDetailsActivity extends AppCompatActivity {
                 startIntent.putExtra("ROOM_SELECTED", roomsTestOnly);//pass room selected ID
                 String roomFromPreviousActivityTest = getIntent().getExtras().getString("ROOM_NAME");
                 System.out.println("THE USER ROOM IS "+roomFromPreviousActivityTest);
+                String Timeid = getIntent().getExtras().getString("TIME_ID");
                 System.out.println("THE USER TIME SLOT ID IS"+getIntent().getExtras().getString("TIME_ID"));
 
+                //THIS VALUE RIGHT HERE IS NULL FIND A WAY TO GET THE USER ID FROM THE CHOOSECAMPUS ACTIVITY TO HERE
+                //CREATING AN INTENT.PUTEXTRA() METHOD SEEMED TO WORK TO THE PAGE THAT IT POINTING TO ASSIGNMENT IN THE BUTTON LISTENERS FOR BOTH PAGES
+                //ASSIGN THE VALUE TO USERID
+                //IF YOU FIND THE ID AND PAST IT IN THE USER ID VARIABLE IT SHOULD WORK
+                String userId=""; //THIS IS THE VARIABLE WE NEED TO GET THE USER ID TO
                 System.out.println("THE USER ID IS "+getIntent().getExtras().getString("REAL_ID"));
+
+                JSONObject params2 = new JSONObject();
+                try {
+                    params2.put("roomId",roomFromPreviousActivityTest);
+                    params2.put("timeslotId", Timeid);
+                    params2.put("userId", userId);
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                String tag_json_obj = "json_obj_req";
+
+                String url = "http://ksustudyroom.azurewebsites.net/api/reservation/addreservation";
+
+
+
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                        url, params2,
+                        new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                //Log.d(TAG, response.toString());
+                                //pDialog.hide();
+                            }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //VolleyLog.d(TAG, "Error: " + error.getMessage());
+                        //pDialog.hide();
+                    }
+                }) {
+
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        return headers;
+                    }
+
+                };
+
+                AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
 
 
                 startActivity(startIntent);
